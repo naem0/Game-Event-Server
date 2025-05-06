d85f937a890253c721fbd668ae6d49c0d8cf253c
MD Naem <121675549+naem0@users.noreply.github.com>
2025-05-06 18:50:20 +0600

    fix: remove unnecessary limits configuration for body size in vercel.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import tournamentRoutes from './routes/tournamentRouter.js';
