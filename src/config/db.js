9bd72ab654ab48bfc829a7923adb18ae80b01263
MD Naem <121675549+naem0@users.noreply.github.com>
2025-05-23 13:14:42 +0600

    fix: update MongoDB connection string to use hardcoded URI

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ console.log(process.env.MONGO_URI,"Connecting to MongoDB...");
 console.log(process.env);
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://mmnayeem408:znunvul2ojlcIF4S@cluster0.sqqija3.mongodb.net/");
         console.log("✅ MongoDB Connected");
     } catch (error) {
         console.error("❌ MongoDB Connection Failed:", error);
